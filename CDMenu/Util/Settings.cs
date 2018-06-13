a86c33457ce6c62ad1e659aad0418087fa53d242
barryntklc <barryn.chun@gmail.com>
2018-06-13 00:36:21 -1000

    re-added Settings.cs

---
+++ CDMenu/Util/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDMenu.Util
+{
+    class Settings
+    {
+        public String CONTENT_DIR = @""; //folder containing content - KR, CN, JP, EA (Korean, Chinese, Japanese, East Asia)
+        public String MENU_DIR = @"menus"; //folder containing menus
+        public String WORKING_DIR = @".";
+        public String MAIN_MENU_FILE = @"menu.json";
+
+        public Settings()
+        {
+
+        }
+    }
+}
