3c9942881bcb67ca874ae5e8dbd468e3132d816f
spot2 <micro@hawaii.edu>
2018-06-13 11:39:33 -1000

    adding untracked items

---
+++ CDMenu/Util/ItemNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CDMenu.Util
+{
+    //https://stackoverflow.com/questions/1438198/how-to-inherit-from-the-treenode-class
+    class ItemNode : TreeNode
+    {
+        public String CALLNO { get; set; }
+        public String PRIMARY_TITLE { get; set; }
+        public String SECONDARY_TITLE { get; set; }
+        public String DESCRIPTION { get; set; }
+
+        public String LAUNCH_PATH { get; set; }
+        public String IMAGE_PATH { get; set; }
+        public String MEDIA_REQUIRED { get; set; }
+
+
+    }
+}
